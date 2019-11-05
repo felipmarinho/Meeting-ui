85e2b318ae8f6e187e264238eea86dc79aa70f3e
Felipe Marinho <felipmarinho@gmail.com>
2019-11-05 20:10:44 -0300

    Adding a component class and adding babel-proposal-class-properties

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from 'react'; // Sempre que eu usar a sintaxe jsx (html) o React precisa estar presente no arquivo.
 import './App.css';
 
-import draw_001 from './assets/draw_001.png';
+import TechList from './components/TechList';
 
 function App() {
-    return <img width="200" src={draw_001} />
+    return <TechList />
 }
 
 export default App;
